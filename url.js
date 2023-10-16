e0d03d21ba7bc8e9dcb9baa0ab8d1e710d72d8b6
Sonarpratik <sonarpratik02.ps@gmail.com>
2023-10-16 11:17:03 +0530

    frontURL module.exports

--- url.js
+++ url.js
@@ -1,2 +1,4 @@
-export const url="https://13.233.173.236/"
-export const frontURL="https://www.siddhisarees.com/"
+const url="https://13.233.173.236/"
+const frontURL="https://www.siddhisarees.com/"
+
+module.exports = {url,frontURL};

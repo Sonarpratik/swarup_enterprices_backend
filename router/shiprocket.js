2e934d1fc0bbf7e704c7291c96631eb3306711fc
Sonarpratik <sonarpratik02.ps@gmail.com>
2024-10-18 16:57:39 +0530

    req bopdy

--- router/shiprocket.js
+++ router/shiprocket.js
@@ -31,9 +31,10 @@ router.post("/api/v1/delivery-notifications", async (req, res) => {
   
 router.post("/api/notification-webhook", async (req, res) => {
   try {
-    console.log("req.body?.payload?.payment",req.body?.payload?.payment)
-    console.log("req.body?.payload?.order",req.body?.payload?.order)
-    res.status(200).json(req.body);
+    console.log("req.body?.payload?.payment",req)
+    console.log("req.body?.payload?.payment",req?.body)
+
+    res.status(200).json("done");
   } catch (err) {
     console.log(err);
     res.status(401).json(err);
@@ -42,8 +43,8 @@ router.post("/api/notification-webhook", async (req, res) => {
 });
 router.get("/api/notification-webhook", async (req, res) => {
   try {
-    console.log("req.body?.payload?.payment",req.body?.payload?.payment)
-    console.log("req.body?.payload?.order",req.body?.payload?.order)
+    console.log("req.body?.payload?.payment",req.body)
+
     res.status(200).json("done");
     // res.status(200).json(req.body?.payload?.payment);
   } catch (err) {
